6b0efcd38c3a2059038ca63cde04912f65708209
Alex Wang <ice_b0und@hotmail.com>
2021-10-17 02:40:39 +0800

    fix mount

--- build_disk_image.sh
+++ build_disk_image.sh
@@ -3,7 +3,8 @@ set -xue
 EFI_SHELL=$1
 loop_device=$(udisksctl loop-setup -f disk.raw | python3 -c "import sys; print(sys.stdin.read().split()[-1][:-1])")
 esp_partition=${loop_device}p1
-mount_path=$(udisksctl mount -b "${esp_partition}" | python3 -c "import sys; print(sys.stdin.read().split()[-1])")
+udisksctl mount -b "${esp_partition}"
+mount_path=$(mount | grep "${esp_partition}" | cut -d ' ' -f 3)
 rm -rf "${mount_path}"/EFI "${mount_path}"/boot_loader.efi "${mount_path}"/startup.nsh
 mkdir -p "${mount_path}"/EFI/Shell
 cp "${EFI_SHELL}" "${mount_path}"/EFI/shell/bootx64.efi
